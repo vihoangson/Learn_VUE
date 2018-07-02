6230ffdf9da48940734f19a81945828207563b64
hoang_son <vihoangson@gmail.com>
2018-07-02 22:32:41 +0700

    custome book

--- js/app.js
+++ js/app.js
@@ -2,58 +2,90 @@ new Vue({
     el: '#app',
     data: {
         isShowingCart: false,
+        isComment: false,
 
         cart: {
             items: []
         },
 
         products: [
-            {
-                id: 1,
-                name: 'MacBook Pro (15 inch)',
-                description: 'This laptop has a super crisp Retina display. Yes, we know that it\'s overpriced...',
-                price: 2999,
-                inStock: 50
-            },
-            {
-                id: 2,
-                name: 'Samsung Galaxy Note 7',
-                description: 'Unlike the overpriced MacBook Pro, we\'re selling this one a bit cheap, as we heard it might explode...',
-                price: 299,
-                inStock: 755
-            },
-            {
-                id: 3,
-                name: 'HP Officejet 5740 e-All-in-One-printer',
-                description: 'This one might not last for so long, but hey, printers never work anyways, right?',
-                price: 149,
-                inStock: 5
-            },
-            {
-                id: 4,
-                name: 'iPhone 7 cover',
-                description: 'Having problems keeping a hold of that phone, huh? Ever considered not dropping it in the first place?',
-                price: 49,
-                inStock: 42
-            },
-            {
-                id: 5,
-                name: 'iPad Pro (9.7 inch)',
-                description: 'We heard it\'s supposed to be pretty good. At least that\'s what people say.',
-                price: 599,
-                inStock: 0
-            },
-            {
-                id: 6,
-                name: 'OnePlus 3 cover',
-                description: 'Does your phone spend most of its time on the ground? This cheap piece of plastic is the solution!',
-                price: 19,
-                inStock: 81
-            }
+        {
+            id: 1,
+            name: 'MacBook Pro (15 inch)',
+            description: 'This laptop has a super crisp Retina display. Yes, we know that it\'s overpriced...',
+            price: 2999,
+            inStock: 50
+        },
+        {
+            id: 2,
+            name: 'Samsung Galaxy Note 7',
+            description: 'Unlike the overpriced MacBook Pro, we\'re selling this one a bit cheap, as we heard it might explode...',
+            price: 299,
+            inStock: 755
+        },
+        {
+            id: 3,
+            name: 'HP Officejet 5740 e-All-in-One-printer',
+            description: 'This one might not last for so long, but hey, printers never work anyways, right?',
+            price: 149,
+            inStock: 5
+        },
+        {
+            id: 4,
+            name: 'iPhone 7 cover',
+            description: 'Having problems keeping a hold of that phone, huh? Ever considered not dropping it in the first place?',
+            price: 49,
+            inStock: 42
+        },
+        {
+            id: 5,
+            name: 'iPad Pro (9.7 inch)',
+            description: 'We heard it\'s supposed to be pretty good. At least that\'s what people say.',
+            price: 599,
+            inStock: 0
+        },
+        {
+            id: 6,
+            name: 'OnePlus 3 cover',
+            description: 'Does your phone spend most of its time on the ground? This cheap piece of plastic is the solution!',
+            price: 19,
+            inStock: 81
+        }
+        ],
+        books:[
+        {
+            title: "Thép đã tôi thế đấy",
+            inStock: 10
+
+        },
+        {
+            title: "Đường dẫn tới khung thành",
+            inStock: 10
+
+        },
+        {
+            title: "Bảy viên ngọc rồng",
+            inStock: 10
+
+        },
+        {
+            title: "Doreamon",
+            inStock: 10
+
+        },
+        {
+            title: "Dũng sỹ hecman",
+            inStock: 10
+
+        },
+
         ]
     },
 
     filters: {
+        formatBook:function (value){
+            return value+" Cuốn sách";
+        },
         currency: function(value) {
             var formatter = new Intl.NumberFormat('en-US', {
                 style: 'currency',
@@ -66,6 +98,17 @@ new Vue({
     },
 
     methods: {
+        buyBook: function (book){            
+            if(book.inStock >0){                
+                book.inStock--;                
+            }
+        },
+        removeBook: function (removeBook){
+            var index = this.books.indexOf(removeBook);
+            this.books.splice(index,1);
+        },
+        
+
         addProductToCart: function(product) {
             var cartItem = this.getCartItem(product);
 
@@ -125,7 +168,7 @@ new Vue({
     },
 
     computed: {
-        cartTotal: function() {
+        cartTotal: function() {            
             var total = 0;
 
             this.cart.items.forEach(function(item) {
