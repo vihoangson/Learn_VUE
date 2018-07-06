24f2f10dfdf7312d26bea0120f6ddbf1be10ff1f
hoang_son <vihoangson@gmail.com>
2018-07-06 23:54:48 +0700

    Lesson 065 Using lifecycle hooks

--- js/app.js
+++ js/app.js
@@ -1,16 +1,51 @@
 var vm = new Vue({
+
     template : `
     <h1>template</h1>
     <p>Content</p>
     `,
     data:{
         message : "Hello world"
-    } 
-});
+    }, 
+    beforeCreate:function(){
+        alert('beforeCreate');
+    },
+    created:function(){
+        alert('created');
+    },
+    beforeMount:function(){
+        alert('beforeMount');
+    },
+    mounted:function(){
+        alert('mounted');
+    },
+    beforeUpdate:function(){
+        alert('beforeUpdate');
+    },
+    updated:function(){
+        alert('updated');
+    },
+    activated:function(){
+        alert('activated');
+    },
+    deactivated:function(){
+        alert('deactivated');
+    },
+    beforeDestroy:function(){
+        alert('beforeDestroy');
+    },
+    destroyed:function(){
+        alert('destroyed');
+    },
+    errorCaptured:function(){
+        alert('errorCaptured');
+    },
+},
+);
 
-//vm.$mount('#app');
-vm.$mount();
+setTimeout(function(){
+    alert(111);
+    vm.$mount("#app");
+    //vm.$data.message = '2112';
+},4000);
 
-console.log(vm.$el);
-
-document.getElementById('app').appendChild(vm.$el);
