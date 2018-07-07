d74baff38bcbc2a2a2e80083e12f02c61b34c0b5
hoang_son <vihoangson@gmail.com>
2018-07-07 15:09:24 +0700

    Lesson 077 Creating a component in the project

---
+++ sample-project/src/Thongbao.vue
@@ -0,0 +1,22 @@
+<template>
+	<div >
+		<div class="alert alert-success">{{msg}}</div>
+		<div class='adfaf'>ádfdsf</div>
+	</div>
+
+</template>
+
+<script>
+export default {
+	name: 'thongbao',
+	data () {
+		return {
+			msg: 'Day la thong bao'
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- sample-project/src/main.js
+++ sample-project/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Thongbao from './Thongbao.vue'
+
+Vue.component('thongbao',Thongbao);
 
 new Vue({
   el: '#app',
