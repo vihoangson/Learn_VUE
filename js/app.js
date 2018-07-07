d590535d005a1b19efb7f63d6262009b90a8b375
hoang_son <vihoangson@gmail.com>
2018-07-07 14:11:53 +0700

    Lesson 074 Why the data property must be a function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
-var vm = new Vue({
-    el: '#app',
-    data: {
-        strings: ""
-    }
-        
+Vue.component ("couter",{
+
+	data:function(){
+		return {
+			num:0
+		}
+	},
+	template:`
+	<button class='btn btn-primary' @click='num++'>{{num}}</button>
+	`
+});
+var data_out = {num:0};
+Vue.component ("couter_data_out",{
+
+	data:function(){
+		return data_out;
+	},
+	template:`
+	<button class='btn btn-success' @click='num++'>{{num}}</button>
+	`
 });
 
-setTimeout(function(){
-    vm.strings = 'hello';
 
-},2000);
+var vm = new Vue({
+	el: '#app'          
+});
